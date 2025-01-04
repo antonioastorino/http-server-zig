3ac35567647f27d8b373eddd25a86ff34bab8282
Antonio Astorino <antonio.astorino.phd@gmail.com>
2025-01-04 20:07:31 +0100

    4 hours just to print a stupid string

--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,20 @@ pub fn main() !void {
     const self_addr = try std.net.Address.resolveIp("0.0.0.0", 4206);
     std.debug.print("address = {}\n", .{self_addr});
     var listener = try self_addr.listen(.{ .reuse_address = true });
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    var allocator = gpa.allocator();
     while (true) {
-        const conn = listener.accept();
+        const conn = try listener.accept();
+        const reader = conn.stream.reader();
         std.debug.print("connection accepted with {any}\n", .{conn});
+        while (true) {
+            const line = try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', 1024);
+            defer allocator.free(line.?);
+            for (line.?) |ch| {
+                std.debug.print("{c}", .{ch});
+            }
+            std.debug.print("{s}", .{"\n"});
+        }
     }
 }

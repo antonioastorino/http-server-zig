66d646c7722cad513a50732e81cd9ebe8825c9a1
Antonio Astorino <antonio.astorino.phd@gmail.com>
2025-01-05 08:42:37 +0100

    Handle EOF and connection closed by client

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const self_addr = try std.net.Address.resolveIp("0.0.0.0", 4206);
+    const self_addr = try std.net.Address.resolveIp("127.0.0.1", 8081);
     std.debug.print("address = {}\n", .{self_addr});
     var listener = try self_addr.listen(.{ .reuse_address = true });
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
@@ -12,12 +12,23 @@ pub fn main() !void {
         const reader = conn.stream.reader();
         std.debug.print("connection accepted with {any}\n", .{conn});
         while (true) {
-            const line = try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', 1024);
-            defer allocator.free(line.?);
-            for (line.?) |ch| {
-                std.debug.print("{c}", .{ch});
+            const bytes = try reader.readUntilDelimiterOrEofAlloc(allocator, '\n', 1024);
+            if (bytes) |line| {
+                defer allocator.free(line);
+                for (line) |ch| {
+                    std.debug.print("{c}", .{ch});
+                }
+                std.debug.print("{s}", .{"\n"});
+                // NOTE: removing this if statement will make the browser hang.
+                // By stopping to load the page you will trigger the closing of the connection from the client side.
+                if (line[0] == '\r') {
+                    std.debug.print("Header end\n", .{});
+                    break;
+                }
+            } else {
+                std.debug.print("Connection closed by the client.\n", .{});
+                break;
             }
-            std.debug.print("{s}", .{"\n"});
         }
     }
 }
